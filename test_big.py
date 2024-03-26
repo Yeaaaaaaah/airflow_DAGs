227f8bba7e0ae1fbdb8b96a6b6f3b8f2bc23fed5
curidom4 <126433780+Yeaaaaaaah@users.noreply.github.com>
2024-03-26 22:58:39 +0900

    Update test_big.py
    gcp_con 을 google_bigguery 로 변경

--- test_big.py
+++ test_big.py
@@ -39,7 +39,7 @@ def load_to_bigquery():  # 함수 이름 및 설명 변경
 
         insert_job = BigQueryInsertJobOperator(
             task_id='insert_current_time_to_bigquery',  # 태스크 이름 변경
-            gcp_conn_id='google_cloud_platform',  # GCP 연결 ID 변경
+            gcp_conn_id='google_bigguery',  # GCP 연결 ID 변경
             project_id=project_id,
             location='asia-northeast2',  # BigQuery 테이블이 있는 리전을 입력하세요.
             dataset_id=dataset_id,
